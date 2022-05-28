e64047854e194fbb12dad0fd2d7db7358e7f5f13
logbaby <96885996+loganbaby@users.noreply.github.com>
2022-05-28 12:04:26 +0300

    new release, debug will not change

---
+++ build/releaseMinSize/qtcsettings.cmake
@@ -0,0 +1,8 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_BUILD_TYPE" "MinSizeRel" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/logbaby/Qt5.12.12/Tools/QtCreator/share/qtcreator/package-manager/auto-setup.cmake" CACHE "PATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/home/logbaby/Qt5.12.12/5.12.12/gcc_64/bin/qmake" CACHE "STRING" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/home/logbaby/Qt5.12.12/5.12.12/gcc_64" CACHE "STRING" "" FORCE)
+set("CMAKE_C_COMPILER" "/usr/bin/clang" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/usr/bin/clang++" CACHE "STRING" "" FORCE)
