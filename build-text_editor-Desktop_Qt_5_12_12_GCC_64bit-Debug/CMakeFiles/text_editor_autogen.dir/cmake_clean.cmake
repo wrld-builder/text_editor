763cf0e441cf2733bb4df931655cc5730447cc4a
logbaby <96885996+loganbaby@users.noreply.github.com>
2022-05-31 10:28:03 +0300

    Delete build-text_editor-Desktop_Qt_5_12_12_GCC_64bit-Debug directory

--- build-text_editor-Desktop_Qt_5_12_12_GCC_64bit-Debug/CMakeFiles/text_editor_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/text_editor_autogen"
-  "text_editor_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/text_editor_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
